8b37464ad7696d9dca0ef8973f9318fcad540078
Joffrey <joyjoffrey@gmail.com>
2022-05-18 22:07:10 +0200

    refactor html and work on css

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
 const ul = document.getElementById('show');
 const open = document.getElementById('open');
 const links = document.querySelectorAll('.menu-link');
-
-if (ul.classList.contains('show')) {
-  ul.classList.remove('show');
-}
-
-links.forEach((link) => {
-  link.addEventListener('click', () => {
-    ul.classList.remove('show');
-  });
-});
-
-open.addEventListener('click', () => {
-  ul.classList.add('show');
-});
